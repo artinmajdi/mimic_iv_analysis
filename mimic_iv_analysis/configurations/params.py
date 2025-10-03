c9fceec91ccd0516aa7652c7e6081cdf638e47a5
Artin Majdi <msm2024@gmail.com>
2025-10-02 22:52:54 -0400

    chore(configurations): update default mimic path to artinmajdi's location

--- mimic_iv_analysis/configurations/params.py
+++ mimic_iv_analysis/configurations/params.py
@@ -693,8 +693,8 @@ DEFAULT_STUDY_TABLES_LIST = [
 # Default paths and configurations
 # ============================================================================
 # TODO: Remove all dependencies on DEFAULT_MIMIC_PATH path except for UI path selection default value.
-# DEFAULT_MIMIC_PATH = Path("/Users/artinmajdi/Library/CloudStorage/GoogleDrive-msm2024@gmail.com/My Drive/MIMIC_IV_Dataset")
-DEFAULT_MIMIC_PATH = Path("/Users/pankajvyas/MIMIC_Project/MIMIC-IV-Raw Data")
+DEFAULT_MIMIC_PATH = Path("/Users/artinmajdi/Library/CloudStorage/GoogleDrive-msm2024@gmail.com/My Drive/MIMIC_IV_Dataset")
+# DEFAULT_MIMIC_PATH = Path("/Users/pankajvyas/MIMIC_Project/MIMIC-IV-Raw Data")
 
 
 DEFAULT_NUM_SUBJECTS = 20
